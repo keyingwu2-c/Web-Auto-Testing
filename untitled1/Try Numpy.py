6944f8c58d8ba4ca19e6d4ab98ba8c3f3dee426c
keyingwu2-c <604192985@qq.com>
2018-08-07 21:37:32 +0800

    Numpy
    Numpy

---
+++ untitled1/Try Numpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+from matplotlib import pyplot as plt
+'''
+#print (np.invert(np.array([-13], dtype = np.uint8)))
+print (np.right_shift(27,2))
+print (np.binary_repr(27,width=8))
+print (np.binary_repr(6,width=8))
+print (np.char.add(['hello', 'hi'],[' abc', ' xyz']))
+'''
+
+
+a = np.array([0.25,  1.33,  1,  100])
+b = np.array([1,4,2,100])
+print (np.reciprocal(a))
+print (np.reciprocal(b))
+
+
+
+
+
+
+
+
+
+
+
+
